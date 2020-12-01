fd65f0b15bccbbbf9c672f9e13ccb5cafa489d26
Daniel Holmes <DanielJHolmes@hotmail.co.uk>
2020-12-01 11:32:52 +0000

    Day_01 - Python - Refactor
    Refactored to a generalised, recursive solution.

--- Day_01/Day_01.py
+++ Day_01/Day_01.py
@@ -4,31 +4,35 @@ def read_data(input_file="input.txt"):
     return data
 
 
-def find_pair_with_sum(data, total=2020):
-    for i in range(len(data)):
-        for j in range(i+1, len(data)):
-            if data[i]+data[j] == total:
-                return (data[i], data[j])
+def find_set_with_sum(data, set_size, total=2020):
+    if set_size <= 1:
+        raise Exception("Error: set_size must be atleast 2")
+
+    if set_size == 2:
+        for i in range(len(data)):
+            for j in range(i+1, len(data)):
+                if data[i]+data[j] == total:
+                    return [data[i], data[j]]
+
+    else:
+        for i, val in enumerate(data[:1-set_size]):
+            sub_set = find_set_with_sum(data[i+1:], set_size-1, total-val)
+            if sub_set is not None:
+                return sub_set+[val]
 
 
-def find_triplet_with_sum(data, total=2020):
-    for i in range(len(data)):
-        for j in range(i+1, len(data)):
-            for k in range(j+1, len(data)):
-                if data[i]+data[j]+data[k] == total:
-                    return (data[i], data[j], data[k])
+def get_formatted_product(int_list):
+    product = 1
+    for x in int_list:
+        product *= x
+    return "x".join(map(str, int_list)) + "={}".format(product)
 
 
 if __name__ == "__main__":
     data = read_data()
-    pair = find_pair_with_sum(data)
-    print("Star 1: {}x{}={}".format(pair[0],
-                                    pair[1],
-                                    pair[0]*pair[1]))
 
-    triplet = find_triplet_with_sum(data)
-    product = triplet[0]*triplet[1]*triplet[2]
-    print("Star 2: {}x{}x{}={}".format(triplet[0],
-                                       triplet[1],
-                                       triplet[2],
-                                       product))
+    pair = find_set_with_sum(data, 2)
+    print("Star 1: "+get_formatted_product(pair))
+
+    triplet = find_set_with_sum(data, 3)
+    print("Star 2: "+get_formatted_product(triplet))
