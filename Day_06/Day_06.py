70540542b750a1aad17d94cad8bfacb456854562
Daniel Holmes <DanielJHolmes@hotmail.co.uk>
2020-12-06 12:58:53 +0000

    Day_06 - Star_01 - Python

---
+++ Day_06/Day_06.py
@@ -0,0 +1,15 @@
+def read_declarations(filename="input.txt"):
+    with open(filename, "r") as input_file:
+        groups = input_file.read().strip().split("\n\n")
+    return [[set(person) for person in group.split()] for group in groups]
+
+
+if __name__ == "__main__":
+    answers = read_declarations()
+
+    star_1_answer = 0
+    star_2_answer = 0
+    for group in answers:
+        star_1_answer += len(set.union(*group))
+
+    print("Star 1: {}".format(star_1_answer))
