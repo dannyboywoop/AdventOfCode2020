0d261f2fab70af0f1a9b5e1af740385d63db4bde
Daniel Holmes <DanielJHolmes@hotmail.co.uk>
2020-12-13 10:45:07 +0000

    Day_13 - Star_02 - Python

--- Day_13/Day_13.py
+++ Day_13/Day_13.py
@@ -1,4 +1,5 @@
 from aoc_tools.advent_timer import Advent_Timer
+from numpy import lcm, int64
 
 
 def read_input(filename="input.txt"):
@@ -19,17 +20,42 @@ def earliest_departure(start_time, buses):
         time += 1
 
 
+def deltas_correct(time, buses):
+    for bus_id, delta in buses:
+        if (time + delta) % bus_id > 0:
+            return False
+    return True
+
+
+def prize_time(buses):
+    time = 1
+    interval = 1
+    for i in range(len(buses)):
+        bus_ids_to_check = [bus_id for bus_id, _ in buses[:i+1]]
+        while True:
+            if deltas_correct(time, buses[:i+1]):
+                break
+            time += interval
+        interval = int(lcm.reduce(bus_ids_to_check, dtype=int64))
+    return time
+
+
 if __name__ == "__main__":
     timer = Advent_Timer()
 
     # parse input
-    start_time, buses = read_input("input.txt")
+    start_time, buses = read_input()
     print("Input parsed!")
     timer.checkpoint_hit()
 
     # star 1
     depart_time, bus_id = earliest_departure(start_time, buses)
     print("Star 1: {}".format((depart_time-start_time) * bus_id))
+    timer.checkpoint_hit()
+
+    # star 2
+    star_2_answer = prize_time(buses)
+    print("Star 2: {}".format(star_2_answer))
     timer.checkpoint_hit()
 
     timer.end_hit()
