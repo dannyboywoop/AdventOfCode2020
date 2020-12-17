582845396131a70576356e23bef1d63b478fe0cd
Daniel Holmes <DanielJHolmes@hotmail.co.uk>
2020-12-17 17:06:15 +0000

    Day_17 - Python - Refactor
    Refactored to us a Pocket_Dimension class

--- Day_17/Day_17.py
+++ Day_17/Day_17.py
@@ -2,76 +2,84 @@ from aoc_tools.advent_timer import Advent_Timer
 from itertools import product
 
 
-class Cube:
-    def __init__(self, pos):
-        self.dimensions = len(pos)
-        self._find_neighbours(pos)
+class Pocket_Dimension:
+    def __init__(self, cube_array, dimensions):
+        self.dimensions = dimensions
+        self.active_cubes = {}
+        self.neighbour_deltas = [deltas for deltas in 
+                                 product([-1, 0, 1], repeat=self.dimensions) 
+                                 if any(deltas)]
+        self._initialise_active_cubes(cube_array)
 
-    def _find_neighbours(self, pos):
-        self.neighbours = set()
-        for deltas in product([-1, 0, 1], repeat=self.dimensions):
-            if any(deltas):
-                neighbour_pos = tuple(map(sum, zip(pos, deltas)))
-                self.neighbours.add(neighbour_pos)
+    def _initialise_active_cubes(self, cube_array):
+        padding = tuple([0]*(self.dimensions-2))
+        for i, row in enumerate(cube_array):
+            for j, val in enumerate(cube_array[i]):
+                if val == ".":
+                    continue
+                self._add_active_cube((j, i)+padding)
+
+    def _add_active_cube(self, position):
+        self.active_cubes[position] = self._get_neighbours(position)
+
+    def _get_neighbours(self, position):        
+        neighbours = set()
+        for deltas in self.neighbour_deltas:
+            neighbour_pos = tuple(map(sum, zip(position, deltas)))
+            neighbours.add(neighbour_pos)
+        return neighbours
+
+    def _count_active_neighbours(self):
+        active_neighbour_count = {active_cube: 0 
+                                  for active_cube in self.active_cubes}
+        for neighbours in self.active_cubes.values():
+            for neighbour in neighbours:
+                if neighbour in active_neighbour_count:
+                    active_neighbour_count[neighbour] += 1
+                else:
+                    active_neighbour_count[neighbour] = 1
+        return active_neighbour_count
+    
+    def _update_active_cubes(self):
+        active_neighbour_count = self._count_active_neighbours()
+        for pos, count in active_neighbour_count.items():
+            if pos in self.active_cubes:
+                if not (count == 2 or count == 3):
+                    self.active_cubes.pop(pos)
+            else:
+                if count == 3:
+                    self._add_active_cube(pos)
+
+    def perform_boot_process(self):
+        for _ in range(6):
+            self._update_active_cubes()
 
 
-def parse_cubes(cube_array, dimensions):
-    active_cubes = {}
-    padding = tuple([0]*(dimensions-2))
-    for i, row in enumerate(cube_array):
-        for j, val in enumerate(cube_array[i]):
-            if val == ".":
-                continue
-            active_cubes[(j, i)+padding] = Cube((j, i)+padding)
-    return active_cubes
-
-
-def read_active_cubes(dimensions, filename="input.txt"):
+def read_cube_array(filename="input.txt"):
     with open(filename, "r") as file:
         cube_array = [line.strip() for line in file]
-    return parse_cubes(cube_array, dimensions)
-
-
-def count_active_neighbours(active_cubes):
-    active_neighbour_count = {active_cube: 0 for active_cube in active_cubes}
-    for active_cube in active_cubes.values():
-        for neighbour_pos in active_cube.neighbours:
-            if neighbour_pos in active_neighbour_count:
-                active_neighbour_count[neighbour_pos] += 1
-            else:
-                active_neighbour_count[neighbour_pos] = 1
-    return active_neighbour_count
-
-
-def update_active_cubes(active_cubes):
-    active_neighbour_count = count_active_neighbours(active_cubes)
-    for pos, count in active_neighbour_count.items():
-        if pos in active_cubes:
-            if not (count == 2 or count == 3):
-                active_cubes.pop(pos)
-        else:
-            if count == 3:
-                active_cubes[pos] = Cube(pos)
-
-
-def perform_boot_process(active_cubes):
-    for _ in range(6):
-        update_active_cubes(active_cubes)
-    return len(active_cubes)
+    return cube_array
 
 
 if __name__ == "__main__":
     timer = Advent_Timer()
+    
+    # parse input
+    cube_array = read_cube_array()
+    print("Input parsed!")
+    timer.checkpoint_hit()
 
     # star 1
-    active_cubes = read_active_cubes(dimensions=3)
-    star_1_answer = perform_boot_process(active_cubes)
+    pocket_dimension_3d = Pocket_Dimension(cube_array, dimensions=3)
+    pocket_dimension_3d.perform_boot_process()
+    star_1_answer = len(pocket_dimension_3d.active_cubes)
     print("Star 1: {}".format(star_1_answer))
     timer.checkpoint_hit()
 
     # star 2
-    active_cubes = read_active_cubes(dimensions=4)
-    star_2_answer = perform_boot_process(active_cubes)
+    pocket_dimension_4d = Pocket_Dimension(cube_array, dimensions=4)
+    pocket_dimension_4d.perform_boot_process()
+    star_2_answer = len(pocket_dimension_4d.active_cubes)
     print("Star 2: {}".format(star_2_answer))
     timer.checkpoint_hit()
 
