0b8e55497d22083c4ac96ef30aecb42d471e3278
Daniel Holmes <DanielJholmes@hotmail.co.uk>
2020-12-24 09:31:49 +0000

    Day_24 - Star_02 - Python

--- Day_24/Day_24.py
+++ Day_24/Day_24.py
@@ -13,6 +13,46 @@ DIRECTIONS = {
 }
 
 
+class Tile_Floor:
+    def __init__(self, black_tiles):
+        self.black_tiles = {black_tile: self._get_neighbours(black_tile)
+                            for black_tile in black_tiles}
+
+    def _add_black_tile(self, position):
+        self.black_tiles[position] = self._get_neighbours(position)
+
+    def _get_neighbours(self, position):
+        x, y = position
+        neighbours = {(x + delta_x, y + delta_y)
+                      for delta_x, delta_y in DIRECTIONS.values()}
+        return neighbours
+
+    def _count_black_neighbours(self):
+        black_neighbour_counts = {black_tile: 0
+                                  for black_tile in self.black_tiles}
+        for neighbours in self.black_tiles.values():
+            for neighbour in neighbours:
+                if neighbour in black_neighbour_counts:
+                    black_neighbour_counts[neighbour] += 1
+                else:
+                    black_neighbour_counts[neighbour] = 1
+        return black_neighbour_counts
+
+    def _update_black_tiles(self):
+        black_neighbour_counts = self._count_black_neighbours()
+        for pos, count in black_neighbour_counts.items():
+            if pos in self.black_tiles:
+                if not (count == 1 or count == 2):
+                    self.black_tiles.pop(pos)
+            else:
+                if count == 2:
+                    self._add_black_tile(pos)
+
+    def evolve(self, num_of_days):
+        for _ in range(num_of_days):
+            self._update_black_tiles()
+
+
 def parse_instructions(instructions):
     return list(findall(INSTRUCTION_REGEX, instructions))
 
@@ -43,6 +83,12 @@ def star_1(instructions_list):
     return black_tiles
 
 
+def star_2(black_tiles):
+    floor = Tile_Floor(black_tiles)
+    floor.evolve(num_of_days=100)
+    return len(floor.black_tiles)
+
+
 if __name__ == "__main__":
     timer = Advent_Timer()
 
@@ -52,6 +98,10 @@ if __name__ == "__main__":
 
     black_tiles = star_1(instructions_list)
     print("Star 1: {}".format(len(black_tiles)))
+    timer.checkpoint_hit()
+
+    star_2_answer = star_2(black_tiles)
+    print("Star 2: {}".format(star_2_answer))
     timer.checkpoint_hit()
 
     timer.end_hit()
